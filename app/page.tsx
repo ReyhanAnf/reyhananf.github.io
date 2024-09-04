c02d9df54277a9e769d159daae0405fea9e47603
Reyhan Andrea Firdaus <77052916+ReyhanAnf@users.noreply.github.com>
2024-09-04 22:58:51 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { FloatingDockMenu } from "./components/ui/floating-dock-menu"
 export default function Home() {
 
   return (
-  <main className="bg-grad px-5 md:px-32 py-5 max-w-full">
+  <main className="bg-grad px-5 md:px-32 py-5 max-w-full" overflow-x-hidden overflow-y-hidden>
     <div className="flex fixed flex-row items-center justify-between w-full z-50">
       <h1 className="text-2xl font-bold px-5 bg-emerald-950 py-2 bg-opacity-15 rounded-md backdrop-blur-lg ">
         <span>{"<"}</span>ReyhanAnf<span>{"/>"}</span>
